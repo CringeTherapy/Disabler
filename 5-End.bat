762e2c38ef58d24747009f877625d8cfe602e3c6
CringeTherapy <99496541+CringeTherapy@users.noreply.github.com>
2022-02-12 20:04:30 -0600

    Update and rename 5-End.bat to 5-fullghost.bat

--- 5-End.bat
+++
@@ -1,1 +0,0 @@
-del %appdata%\.minecraft
---
+++ 5-fullghost.bat
@@ -0,0 +1,7 @@
+del %appdata%\.minecraft
+del %localappdata%\Roblox
+del %appdata%\flashpoint-launcher
+del "%localappdata%\NVIDIA Corporation"
+del %localappdata%\NVIDIA
+del %appdata%\.tlauncher
+del %appdata%\games
