8830374a6f49360495217d70b92bd191233c73d8
CringeTherapy <99496541+CringeTherapy@users.noreply.github.com>
2022-02-12 20:00:46 -0600

    Update and rename ghost.bat to 3-ghost.bat

---
+++ 3-ghost.bat
@@ -0,0 +1,7 @@
+ren %appdata%\.minecraft GoGuardian
+ren %localappdata%\Roblox InfoGraphics
+ren %appdata%\flashpoint-launcher HistoryProjects
+ren "%localappdata%\NVIDIA Corporation" ScienceProjects
+ren %localappdata%\NVIDIA EnglishEssays
+ren %appdata%\.tlauncher CK12
+ren %appdata%\games ElementarySchoolProjects
